8cd419c92fd825a6dd1f269bd3f0852c08c28bdc
tprax <tprax01@gmail.com>
2019-05-08 14:59:17 -0600

    first commit

---
+++ hello_world.rb
@@ -0,0 +1,32 @@
+first_name = 'First Name'
+last_name = 'Last Name'
+world = 'World'
+first_number = 10
+second_number = 2.0
+
+puts 'Please Enter First Name'
+first_name = gets
+puts ''
+puts 'Please Enter Last Name'
+last_name = gets
+puts ''
+
+puts ''
+puts 'Here is your Info'
+puts first_name
+puts last_name
+puts first_number
+puts second_number
+
+number_array = []
+number_array << 1
+number_array << 2
+number_array << 50
+puts number_array[1]
+puts number_array[4]
+
+def hello
+  puts 'Hello World'
+end
+
+hello
