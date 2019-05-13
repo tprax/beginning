06180d99b9afc652292e95b20572b914682b4615
tprax <tprax01@gmail.com>
2019-05-13 12:28:23 -0600

    add new file

---
+++ contact.rb
@@ -0,0 +1,26 @@
+#Contact List
+#Create Contact List
+#Create Menu
+  #1. Create A Contact
+  #2. View All Contacts
+  #3. Delete Contact
+  #4. Edit Contact
+  #5. Exit
+
+def menu
+
+end
+
+def create_contact
+
+end
+
+def list_contacts
+
+end
+
+def delete_contact
+
+end
+
+def edit_contact
