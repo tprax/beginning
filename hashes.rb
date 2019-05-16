0567b9fee0f79838eb3958943823f4df83956450
tprax <tprax01@gmail.com>
2019-05-16 15:37:39 -0600

    displaying hashes

--- hashes.rb
+++ hashes.rb
@@ -19,9 +19,13 @@ puts languages[:language][:ruby][:awesomeness]
 languages[:language][:java][:awesomeness] = -280000
 puts languages[:language][:java][:awesomeness]
 hash = {hero: 'batman', money: '$$$$', color: 'black'}
-puts hash.keys
-hash.delete(:color)
-puts hash.keys
-hash.clear
-puts hash.keys
-puts hash.has_key? :car
+#puts hash.keys
+#hash.delete(:color)
+#puts hash.keys
+#hash.clear
+#puts hash.keys
+#puts hash.has_key? :car
+
+hash.each do |key, value|
+  puts "Key: #{key}, Value: #{value}"
+end
