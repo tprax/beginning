9e3df089156d272a39aae6ea8cf15adb02e55614
tprax <tprax01@gmail.com>
2019-05-16 08:42:39 -0600

    messing with hashes

--- hashes.rb
+++ hashes.rb
@@ -3,3 +3,16 @@ person = {first_name: 'first',last_name: 'last', age: 28}
 puts person[:first_name]
 puts person[:last_name]
 puts person[:age]
+languages = {
+  language: {
+    ruby: {
+      conceived: 1993 ,
+      awesomeness: 9000
+    },
+    java:{
+      conceived: 1991,
+      awesomeness: -2800
+    }
+  }
+}
+puts languages[:language][:ruby][:awesomeness]
