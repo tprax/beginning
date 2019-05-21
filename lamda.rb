a16454d7daaed6b6ef964e8f3dd1649f002f8de8
tprax <tprax01@gmail.com>
2019-05-21 11:55:13 -0600

    blocks

--- lamda.rb
+++ lamda.rb
@@ -1,1 +1,11 @@
 #Blocks, Procs, Lamdas
+#blocks are anything in between from a do/end or {}
+arr = [1,2,3,4,5,6,7,8,9,10]
+arr.each do |num|
+  puts num ** 2
+end
+
+#arr.each{|num| puts num ** 2}
+square = Proc.new do |num|
+  num ** 2
+end
