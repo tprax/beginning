276197a1fc592c901f012d4e3d72a056a41375d3
tprax <tprax01@gmail.com>
2019-05-22 11:16:38 -0600

    procs

--- lamda.rb
+++ lamda.rb
@@ -1,18 +1,23 @@
 #Blocks, Procs, Lamdas
 #blocks are anything in between from a do/end or {}
-arr = [1,2,3,4,5]
-arr.each do |num|
-  puts num ** 2
-end
+@arr = [1,2,3,4,5]
+#@arr.each do |num|
+#  puts num ** 2
+#end
 
 array = [1,2,3,4]
 
-array.each{|num| puts num ** 2}
-square = Proc.new do |num|
-  num ** 2
-end
+#array.each{|num| puts num ** 3}
+#square = Proc.new do |num|
+#  num ** 2
+#end
 
-#square = Proc.new {|num| num ** 2}
+#@arr.each{|num| puts num ** 4}
+#square = Proc.new do |num|
+#  num ** 2
+#end
+
+square = Proc.new {|num| num ** 2}
 num = square.call(8)
 puts num
 
@@ -20,7 +25,8 @@ def square_arr(arr, sq)
   arr.each_with_index do |num, i|
     arr[i] = sq.call(arr[i])
   end
-  arr
+  puts arr
 end
 
 square_arr(array, square)
+square_arr(@arr, square)
